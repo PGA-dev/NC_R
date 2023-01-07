a6fd1eeeb22dd4a804af6b7285f56235e63a3cbe
PGA <paulgadam@gmail.com>
2023-01-07 12:53:37 -0700

    removed old references and imports from setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import CampsitesList from './features/campsites/CampsitesList';
-//import { CAMPSITES } from './app/shared/CAMPSITES';
 import React from 'react';
-//import logo from './logo.svg';
-//import { Counter } from './features/counter/Counter';
-//import { Container, Navbar, NavbarBrand } from 'reactstrap';
-//import NucampLogo from './app/assets/img/logo.png';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
