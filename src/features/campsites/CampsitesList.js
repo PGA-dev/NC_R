3c91c309833b9faa4b4ee20acea8cf2246c7f15a
PGA <paulgadam@gmail.com>
2023-01-11 08:26:47 -0700

    hooks working fully

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,15 +1,19 @@
 //import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import CampsiteCard from "./CampsiteCard";
-import { Row, Col} from 'reactstrap';
-import { selectAllCampsites} from './campsitesSlice';
+import { Row, Col } from 'reactstrap';
+import { selectAllCampsites } from './campsitesSlice';
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteId }) => {
     const campsites = selectAllCampsites();
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col sm='12' xxl='5' className='mt-4' key={campsite.id}>
+                    <Col sm='12'
+                        xxl='5'
+                        className='mt-4'
+                        key={campsite.id}
+                        onClick={() => setCampsiteId(campsite.id)}>
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
