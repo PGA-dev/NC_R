2d0821750404d7896a93a03a919fd88a6dd33882
PGA <paulgadam@gmail.com>
2023-01-11 01:57:41 -0700

    updated button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import CampsitesList from './features/campsites/CampsitesList';
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
