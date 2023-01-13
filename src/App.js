97c28dfb6edc317bc9a0f41da93820406bba0a50
PGA <paulgadam@gmail.com>
2023-01-12 21:03:18 -0700

    added CampsiteDetailPage and updated aps for URLs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+
 import './App.css';
 
 function App() {
@@ -14,6 +16,10 @@ function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='contact' element={<ContactPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
             </Routes>
             <Footer />
         </div>
