443372fd79f5ffba6e81935ced810ffd7c82ed84
PGA <paulgadam@gmail.com>
2023-01-14 11:02:48 -0700

    partners updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='contact' element={<ContactPage />} />
+                <Route path='about' element={<AboutPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
                 <Route
                     path='directory/:campsiteId'
